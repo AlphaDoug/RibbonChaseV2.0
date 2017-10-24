0918f25d6752ec7716350c16908ce9ddf6a351d3
DengPan <DengPanStudio@outlook.com>
2017-10-24 13:53:33 +0800

    加入分享功能以及生命值增加提示框

--- Assets/FlightKit/FlightKit/Scripts/Ads/PlayAds.cs
+++ Assets/FlightKit/FlightKit/Scripts/Ads/PlayAds.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 using Together;
 public class PlayAds : MonoBehaviour {
     public GameObject gameController;
+	public GameObject warningText;
+	public GameObject UICanvas;
 	// Use this for initialization
 	void Start () {
         TGSDK.AdCloseCallback = OnAdClose;
         TGSDK.VideoAdLoadedCallback = OnVideoAdLoaded;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
     public void Ads()
     {
         if (TGSDK.CouldShowAd("FXFLPDOjRrCQSzlEl34"))
@@ -24,6 +22,12 @@ public class PlayAds : MonoBehaviour {
     public void OnAdClose(string ret)
     {
 		gameController.GetComponent<LifeNumCtrl> ().AdsAddLife (5);
+		GameObject warning = Instantiate (warningText)as GameObject;
+		warning.transform.SetParent (UICanvas.transform);
+		warning.GetComponent<RectTransform>().localPosition = new Vector3 (-23, 864.9999f, 0);
+		warning.GetComponent<RectTransform>().localScale = new Vector3 (1, 1, 1);
+		warning.GetComponent<RectTransform>().localRotation = new Quaternion (0, 0, 0,0);
+
     }
 
     public void OnVideoAdLoaded(string ret)
